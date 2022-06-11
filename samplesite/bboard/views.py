7734aad83b5a7259291b6b34fbebbbdbe5b09953
vladimir <sudilovskiyv@mail.ru>
2022-06-11 18:20:44 +0300

    bboard application created, admin panel set up

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Bb
 
+
 def index(request):
-    s = 'Список объявлений\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    bbs = Bb.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs})
