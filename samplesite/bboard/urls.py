8e55f867ef422b5fcfdc635b5b7a5d964111ea88
vladimir <sudilovskiyv@mail.ru>
2022-06-12 17:32:00 +0300

    added basic sample and style

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, by_rubric, BbCreateView
 
 urlpatterns = [
-    path('', index),
-]
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'), # параметризованный url-параметр
+    path('', index, name='index'),
+    path('add/', BbCreateView.as_view(), name='add'),
+] # name - обратное разрешение интернет-адресов
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from .models import Bb
+from django.urls import reverse_lazy
+from .models import Bb, Rubric
+from django.views.generic.edit import CreateView
+from .forms import BbForm
 
 
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    content = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', content)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
